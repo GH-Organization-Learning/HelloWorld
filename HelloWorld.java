4a816a19b0dc76c3195de07e47cd0980816a14e6
ViniciusPeralta <vinicius-peralta@hotmail.com>
2025-02-18 20:37:21 -0300

    feat: add date to hello world

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,3 +1,9 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 void main() {
     System.out.println("Hello World!");
+    System.out.println("Date: " + LocalDateTime.now()
+            .format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"))
+    );
 }
